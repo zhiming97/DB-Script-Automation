7d2ed17e89709b5c2cea97d242005ba60e529121
Diamondhands <97498951+zhiming97@users.noreply.github.com>
2023-08-06 16:16:32 +0800

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -3,43 +3,34 @@ from datetime import datetime
 import os
 import streamlit as st
 
-def replace_datetime_with_sysdate(content):
+def DB_SCIRPT_CLEANUP(db):
     try:
-        # Regular expression to find datetime format strings like '2023-08-08 13:00:21'
-        datetime_pattern = r'\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}'
-
-        # Find all occurrences of the datetime pattern in the text
-        matches = re.findall(datetime_pattern, content)
-
-        # Replace each occurrence with 'SYSDATE'
+        datetime_pattern = r"'\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}'"
+        matches = re.findall(datetime_pattern, db)
         for match in matches:
-            formatted_match = datetime.strptime(match, '%Y-%m-%d %H:%M:%S').strftime('SYSDATE')
-            content = content.replace(match, formatted_match)
+            formatted_match = datetime.strptime(match, "'%Y-%m-%d %H:%M:%S'").strftime('SYSDATE')
+            db = db.replace(match, formatted_match)
 
-        return content
+        return db
     except Exception as e:
         return f"Error: {e}"
 
 def main():
-    st.title('Upload and Convert Datetime to SYSDATE')
+    st.title('DB Script Preparation Tool')
 
     uploaded_file = st.file_uploader('Upload a dbscript file', type=['sql'])
 
     if uploaded_file is not None:
         content = uploaded_file.read().decode('utf-8')
+        modified_db_script = DB_SCIRPT_CLEANUP(content)
 
-        # Replace datetime with SYSDATE
-        modified_content = replace_datetime_with_sysdate(content)
+        st.text(modified_db_script)
 
-       # Display the modified content
-        st.text(modified_content)
-
-        # Download link for the modified file
-        modified_bytes = modified_content.encode('utf-8')
-        st.download_button('Download Modified File', data=modified_bytes, file_name='modified_script.sql', mime='text/sql')
+        new_db_script = modified_db_script.encode('utf-8')
+        st.download_button('Download Your New DB Script', data=new_db_script, file_name='New_DB_Script.sql', mime='text/sql')
 
     else:
-        st.info('Please upload a dbscript file.')
+        st.info('Please Upload the DB Script that you would like to Convert.')
 
 if __name__ == '__main__':
     main()
